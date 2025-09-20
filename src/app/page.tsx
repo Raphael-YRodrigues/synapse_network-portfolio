3070322cbaa193ad93c36e6ee501965c0adc8bf8
Raphael <raphael.workplace@gmail.com>
2025-09-20 14:58:06 +0100

    Made the main container (3D canvas) relative and added a new 2D absolute div to float on top of it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
-import Scene from './components/Scene' // Import the Scene
+import Scene from './components/Scene'
 
 export default function Home() {
   return (
-    <main className="h-screen w-screen bg-gray-900">
-      {/* This will be our 3D canvas */}
+    <main className="relative h-screen w-screen bg-gray-900">
+      {/* 3D Scene - The Background Layer */}
       <Scene />
+
+      {/* 2D UI - The Foreground Layer */}
+      <div className="pointer-events-none absolute left-0 top-0 z-10 flex h-full w-full flex-col items-center justify-center text-white">
+        <h1 className="text-5xl font-bold">Raphael Rodrigues</h1>
+        <p className="mt-4 text-lg">Creative Developer Portfolio</p>
+      </div>
     </main>
   )
 }
