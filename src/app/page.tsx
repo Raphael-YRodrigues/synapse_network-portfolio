ef21e63ef542f4e09d8c3fe8c1ed74ea8f51563f
Raphael <raphael.workplace@gmail.com>
2025-09-20 14:49:02 +0100

    Adding the new Scene.tsx to the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Scene from './components/Scene' // Import the Scene
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gray-900 text-white">
-      <h1 className="text-5xl font-bold">Raphael Rodrigues</h1>
-      <p className="mt-4 text-lg">Creative Developer Portfolio</p>
+    <main className="h-screen w-screen bg-gray-900">
+      {/* This will be our 3D canvas */}
+      <Scene />
     </main>
   )
 }
